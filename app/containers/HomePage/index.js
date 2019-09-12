6b10596cb694f7652bdd298d63fd2d0f10cd6c1c
Kit Masaracchia <kit.masaracchia@gmail.com>
2019-09-12 00:24:40 -0600

    create, render logo

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -6,13 +6,25 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import styled from 'styled-components';
+import LogoBase from 'components/Logo';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  padding: 70px 20px 0;
+`;
+
+const Logo = styled(LogoBase)`
+  display: block;
+  padding-top: 38px;
+  margin: 0 auto;
+`;
 
 export default function HomePage() {
   return (
-    <h1>
-      <FormattedMessage {...messages.header} />
-    </h1>
+    <Wrapper>
+      <Logo />
+    </Wrapper>
   );
 }
---
+++ app/theme.js
@@ -0,0 +1,6 @@
+export const colors = {
+  brand: '#64FD66',
+  font: '#032067',
+  highlight: '#F8E71C',
+  lowlight: '#D7D7D7',
+};
